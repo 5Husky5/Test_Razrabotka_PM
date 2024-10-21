bdcecffbe6f72f8333769948a603e8eb257678d2
5Husky5 <zhenya.bondarchuk05@gmail.com>
2024-10-21 14:23:33 +0300

    Координаты точек

---
+++ Kr1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kr1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1, x2, y1, y2;
+            Random random = new Random();
+            int minx = 0;
+            int maxx = 100;
+            int miny = 0;
+            int maxy = 100;
+
+            double x1 = random.Next(minx, maxx + 1);
+            double x2 = random.Next(minx, maxx + 1);
+            double y1 = random.Next(minx, maxy + 1);
+            double y2 = random.Next(minx, maxy + 1);
+
+           
+        }
+    }
+}
